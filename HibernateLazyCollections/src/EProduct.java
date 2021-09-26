6c8d2ee627109fd5e87354cd2414c566b4c5ecb4
Shub <shubrota@gmail.com>
2021-09-26 19:38:59 +0530

    Hibernate and JSP Labs

--- HibernateLazyCollections/src/EProduct.java
+++ HibernateLazyCollections/src/EProduct.java
@@ -4,18 +4,27 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
-import java.util.Map;
+
+
 
 public class EProduct {
-    private long ID;
+	private long ID;
     private String name;
     private BigDecimal price;
     private Date dateAdded;
     private List<Color> colors;
-    private Collection<ScreenSizes> screenSizes;
-    private Set<OS> os;
-    private Map finance;
+    private Set<OS> OS;
     
+    public Set<OS> getOs() {
+		return OS;
+	}
+
+	public void setOs(Set<OS> oS) {
+		this.OS = oS;
+	}
+	private Set<Finance> finance;
+   // private PDescription pdescrip;
+    private Collection<ScreenSizes> screenSizes;
     public EProduct() {
             
     }
@@ -24,18 +33,18 @@ public class EProduct {
     public String getName() { return this.name;}
     public BigDecimal getPrice() { return this.price;}
     public Date getDateAdded() { return this.dateAdded;}
-    public List<Color> getColors() { return this.colors;}
-    public Collection<ScreenSizes> getScreensizes() { return this.screenSizes;}
-    public Set<OS> getOs() { return this.os;}
-    public Map getFinance() { return this.finance;}
+    public List<Color> getColor() { return this.colors;}
+    public Set<Finance> getFinance() { return this.finance;}
+   // public PDescription getPdescrip() { return this.pdescrip;}
+    public Collection<ScreenSizes> getScreenSizes() { return this.screenSizes;}
     
     public void setID(long id) { this.ID = id;}
     public void setName(String name) { this.name = name;}
     public void setPrice(BigDecimal price) { this.price = price;}
     public void setDateAdded(Date date) { this.dateAdded = date;}
-    public void setColors(List<Color> colors) { this.colors = colors;}
-    public void setScreensizes(Collection<ScreenSizes> sizes) { this.screenSizes = sizes;}
-    public void setOs(Set<OS> os) { this.os = os;}
-    public void setFinance(Map finance) { this.finance = finance;}
+    public void setColor(List<Color> colors) { this.colors = colors;}
+    public void setFinance(Set<Finance> finance) { this.finance = finance;}
+    //public void setPdescrip(PDescription pdescrip) { this.pdescrip = pdescrip;}
+    public void setScreenSizes(Collection<ScreenSizes> sizes) { this.screenSizes = sizes;}
 
 }
--- HibernateLazyCollections/src/ProductDetails.java
+++ HibernateLazyCollections/src/ProductDetails.java
@@ -68,15 +68,15 @@ public class ProductDetails extends HttpServlet {
                 for(EProduct p: list) {
                         out.println("ID: " + String.valueOf(p.getID()) + ", Name: " + p.getName() +
                                         ", Price: " + String.valueOf(p.getPrice()) + ", Date Added: " + p.getDateAdded().toString());
-                        PDescription descrip = p.getPdescrip();
-                        out.println("<br>Description:" + descrip.getDescrip());
+                       // PDescription descrip = p.getPdescrip();
+                      //  out.println("<br>Description:" + descrip.getDescrip());
                         out.println("<hr>");
                 }                       
                 out.println("<b>One to Many and Many to One Mapping</b><br>");
                 for(EProduct p: list) {
                         out.println("ID: " + String.valueOf(p.getID()) + ", Name: " + p.getName() +
                                         ", Price: " + String.valueOf(p.getPrice()) + ", Date Added: " + p.getDateAdded().toString());                           
-                        List<Color> colors = p.getColors();
+                        List<Color> colors = p.getColor();
                         out.println("<br>Colors: <ul>");
                         for(Color c: colors) {
                                 out.print("<li>" + c.getName() + "</li>");
